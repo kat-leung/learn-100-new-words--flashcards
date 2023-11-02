fd283280215679e18b1aff1d6ff221f30cc31e71
Kat L <kathyl123@gmail.com>
2023-11-02 15:18:23 -0700

    added hover text to buttons

--- src/Pages/Flashcard.jsx
+++ src/Pages/Flashcard.jsx
@@ -27,15 +27,61 @@ export default function Flashcard({lang}){
     function handleCorrect() {
         correct.push(flashcardData[cardIndex])
         setCardIndex(cardIndex + 1);  
+        setFlip(false)
     }
 
     function handleWrong() {
         incorrect.push(flashcardData[cardIndex])
-        setCardIndex(cardIndex + 1);  
+        setCardIndex(cardIndex + 1);
+        setFlip(false)  
+    }
+
+    const correctText = 
+        <p className="button-text">correct</p>
+
+    const [correctButtonHover, setCorrectButtonHover] = React.useState(false)
+
+    function handleCorrectMouseOver() {
+        setCorrectButtonHover(true)
+    }
+
+    function handleCorrectMouseOut(){
+        setCorrectButtonHover(false)
+    }
+
+    const restartText = 
+        <p className="button-text">restart</p>
+
+    const [restartButtonHover, setRestartButtonHover] = React.useState(false)
+
+    function handleRestartMouseOver() {
+        setRestartButtonHover(true)
+    }
+
+    function handleRestartMouseOut(){
+        setRestartButtonHover(false)
+    }
+
+    const wrongText = 
+        <p className="button-text">wrong</p>
+
+    const [wrongButtonHover, setWrongButtonHover] = React.useState(false)
+
+    function handleWrongMouseOver() {
+        setWrongButtonHover(true)
+    }
+
+    function handleWrongMouseOut(){
+        setWrongButtonHover(false)
+    }
+
+    function restartDeck (){
+        setCardIndex(0)
     }
 
 
-    return (
+
+    return ( 
         <div className="main-content">
             <div className={`flip-card ${flip ? 'flipped' : ''}`} onClick={handleClick}>
                 <div className="flip-card-inner">    
@@ -47,18 +93,26 @@ export default function Flashcard({lang}){
                     </div>
                 </div>
             </div>
-        
             
                 <div className="buttons">
-                        <div className="correct-button" onClick={handleCorrect}>
+                    <div className="button-element">
+                        {correctButtonHover && correctText}
+                        <div className="correct-button" onClick={handleCorrect} onMouseOver={handleCorrectMouseOver} onMouseOut={handleCorrectMouseOut}>
                             <img className="icon" src={right} alt="checkmark with green background"/>
                         </div>
-                        <div className="restart-button" onClick={handleFlipDeck}>
+                    </div>
+                    <div className="button-element">
+                        {restartButtonHover && restartText}  
+                        <div className="restart-button" onClick={restartDeck} onMouseOver={handleRestartMouseOver} onMouseOut={handleRestartMouseOut}>
                             <img className="icon" src={wrong} alt="an x with a red background"/>
                         </div>
-                        <div className="wrong-button" onClick={handleWrong}>
+                    </div>
+                    <div className="button-element">
+                        {wrongButtonHover && wrongText}  
+                        <div className="wrong-button" onClick={handleWrong} onMouseOver={handleWrongMouseOver} onMouseOut={handleWrongMouseOut}>
                             <img className="icon" src={restart} alt="a rounded arrow pointing back"/>
                         </div>
+                    </div>
                 </div>
         </div>
     )
